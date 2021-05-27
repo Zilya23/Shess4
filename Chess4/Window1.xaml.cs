a88eb62845217e41c19aff163ec0205f3bf6bac6
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2021-05-27 22:50:24 +0300

    Изменила способ того, как ставитьт фигуру на доску и создала новое окно

--- Chess4/Window1.xaml.cs
+++ Chess4/Window1.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Chess4
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public Window1 WinNewgame;
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void btnKing_Click(object sender, RoutedEventArgs e)
-        {
-            WinNewgame = new Window1();
-            WinNewgame.Show();
         }
     }
 }
